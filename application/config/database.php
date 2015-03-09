479d26e41eb057f6423dc09928ee103043d924ae
Chintan Shah <chintan@wohlig.com>
2015-03-10 03:59:02 +0530

    database

--- application/config/database.php
+++ application/config/database.php
@@ -49,7 +49,7 @@ $active_group = 'default';
 $active_record = TRUE;
 
 
-
+/*
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'kiran231_dellall';
 $db['default']['password'] = 'chintan123';
@@ -65,7 +65,23 @@ $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
+*/
 
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
+$db['default']['database'] = 'kiran231_dellall';
+$db['default']['dbdriver'] = 'mysql';
+$db['default']['dbprefix'] = '';
+$db['default']['pconnect'] = TRUE;
+$db['default']['db_debug'] = TRUE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = '';
+$db['default']['char_set'] = 'utf8';
+$db['default']['dbcollat'] = 'utf8_general_ci';
+$db['default']['swap_pre'] = '';
+$db['default']['autoinit'] = TRUE;
+$db['default']['stricton'] = FALSE;
 
 /* End of file database.php */
 /* Location: ./application/config/database.php */
